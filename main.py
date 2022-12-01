e872f6771d8e2873f7dd20251e97b43c0a6a6672
sarransulaim <82736275+sarransulaim@users.noreply.github.com>
2022-12-02 00:45:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+from time import time
 import streamlit as st
 from datetime import date
 
@@ -7,16 +9,26 @@ from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
 
-START = "2016-01-01"
+START = "2010-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
+st.set_page_config(layout="wide")
 st.title("Stock Prediction Platform")
-stocks = ("AAPL", "GOOG", "MSFT", "GME", "TSLA", "BTC-USD", "ETH-USD", "DOGE-USD", "SHIB-USD", "TWTR", "META", "RELIANCE.NS", "TATASTEEL.NS", "TATAMOTORS.NS", "TATAPOWER.NS"
+stocks = ("AAPL", "GOOG", "BC94.L",  "MSFT", "GME", "TSLA", "BTC-USD", "ETH-USD", "DOGE-USD", "SHIB-USD", "TWTR", "META", "RELIANCE.NS", "TATASTEEL.NS", "TATAMOTORS.NS", "TATAPOWER.NS"
 , "PEP", "COKE", "IOC.NS")
 selected_stocks = st.selectbox("Select Stock for Prediction", stocks)
 
-n_years = st.slider("Years of Prediction:", 1, 7)
+n_years = st.slider("Years of Prediction:", 1, 10)
 period = n_years * 365
+
+#n_months = st.slider("Months of Prediction:", 1, 12)
+#period1 = n_months * 30
+
+#n_weeks = st.slider("Weeks of Prediction:", 1, 5)
+#period = n_weeks * 7
+
+#n_days = st.slider("Days of Prediction:", 1, 30)
+#period = n_days * 1
 
 @st.cache
 def load_data(ticker):
@@ -29,7 +41,7 @@ data = load_data(selected_stocks)
 data_load_state.text("Loading data....done!")
 
 st.subheader('Raw Data')
-st.write(data.tail())
+st.write(data.tail(10))
 
 def plot_raw_data():
     fig = go.Figure()
@@ -50,12 +62,12 @@ future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
 st.subheader('Forecast Data')
-st.write(forecast.tail())
+st.write(forecast.tail(10))
 
 st.subheader('Forecast Chart')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
-st.subheader('Forcast Components')
+st.subheader('Forecast Components')
 fig2 = m.plot_components(forecast)
 st.write(fig2)
