5623fc3091568d0002a67e79272b36665242a579
sarransulaim <82736275+sarransulaim@users.noreply.github.com>
2022-12-02 18:27:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ st.subheader('Raw Data')
 st.write(data.tail(10))
 
 def plot_raw_data():
-    fig = go.Figure()
+    fig = go.Figure(5, 5)
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
     fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
